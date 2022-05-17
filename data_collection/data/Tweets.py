3a1ec980ede18cad5db9163e7472e58b89cc8776
Alex McCune <alexmccune1224@gmail.com>
2022-05-16 18:08:23 -0700

    cleanup on outdated files

--- data_collection/data/Tweets.py
+++
@@ -1,9 +0,0 @@
-from mongoengine import *
-
-class Tweet(Document):
-    author_id = StringField(required=True)
-    tweet_id = StringField(required=True)
-    text = StringField(required=True) 
-    created_at = DateTimeField(required=True)
-
-
--- data_collection/data/User.py
+++
@@ -1,7 +0,0 @@
-from mongoengine import *
-class TwitterUser(Document):
-    twitter_id = StringField(required=True)
-    username = StringField(required=True)
-    last_updated = DateTimeField(required=True)
-
-
