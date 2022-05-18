c0b8aeb9c8f59df325b323eeb227169329748d92
Alex McCune <alexmccune1224@gmail.com>
2022-05-18 12:57:29 -0700

    added little_t/src as package so serverless can use

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
-import setuptools
+from setuptools import setup, find_packages
 
+VERSION = '0.0.1' 
+DESCRIPTION = 'little-t binaries'
+LONG_DESCRIPTION = 'files and packages needed for little-t'
 
-setuptools.setup(
-    name="little-t-ALEX-MCCUNE",
-    version="0.0.1",
-    author="Alex McCune",
-    author_email="alexmccune1224@gmail.com",
-    description="Little-t code",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-    ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.9",
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="little-t", 
+        version=VERSION,
+        author="Alex McCune",
+        author_email="alexmccune1224@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
 )
